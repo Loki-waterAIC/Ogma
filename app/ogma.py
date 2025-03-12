90d72bc0e9bb7f9f43917442e4445dc571aab63d
Aaron Shackelford <aaron.shackelford@wateraic.com>
2025-03-12 13:25:22 -0700

    ogma file

---
+++ app/ogma.py
@@ -0,0 +1,8 @@
+'''
+ # @ Author: Aaron Shackelford
+ # @ Create Time: 2025-03-12 13:19:04
+ # @ Modified by: Aaron Shackelford
+ # @ Modified time: 2025-03-12 13:19:19
+ # @ Description: <Description>
+ '''
+
