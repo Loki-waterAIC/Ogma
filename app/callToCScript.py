c1963ab4f66bdc305f6e679552eb6c3c7d1dc315
Aaron Shackelford <aaron.shackelford@wateraic.com>
2025-03-18 10:47:04 -0700

    added a visible toggle for word and improced output message

--- app/callToCScript.py
+++ app/callToCScript.py
@@ -2,6 +2,7 @@ import subprocess
 from cscriptErrors import cscriptError
 import RunWordMacro
 
+RUN_SUBPROCESS:bool = False
 RUN_SUBPROCESS:bool = True
 
 def update_doc_properties(doc_path:str) -> None:
@@ -16,26 +17,33 @@ def update_doc_properties(doc_path:str) -> None:
     '''
     
     # set the macro
-    macro:str = r"UpdatePropertiesButton_Click"
+    macro:str = r"UpdateAllFields"
+    visible = True
     
     
     if RUN_SUBPROCESS:
         # run macro as a cscript
         vbs_script = r"app\RunWordMacro.vbs"  # Update with the actual path
+        
+        visible = str(visible)
 
         try:
             result: subprocess.CompletedProcess[str] = subprocess.run(
-                args=[r"cscript", r"/nologo", r"/b", vbs_script, doc_path, macro],
+                args=[r"cscript", r"/nologo", r"/b", vbs_script, doc_path, macro, visible],
                 capture_output=True,
                 text=True,
                 check=True,
             )
-            print(f"Macro ran successfully for {doc_path}:\n", result.stdout)
+            print(f"Macro ran without error for {doc_path}\n\tstdout >>> ", result.stdout)
         except subprocess.CalledProcessError as e:
             raise cscriptError("Error running macro:\n", e.stderr)
         
     else:
         # run as a py script
-        RunWordMacro.run_word_macro(doc_path=doc_path, macro_name=macro)
+        RunWordMacro.run_word_macro(doc_path=doc_path, macro_name=macro, wordVisible=visible)
         
     return
+
+if __name__ == "__main__":
+    # Example usage    
+    update_doc_properties(doc_path=r"data\hidden\1. Revision History.docx")
