fdcc55ecc4829f43f9f47647a5258a4dd0ecb2ac
Aaron Shackelford <aaron.shackelford@wateraic.com>
2025-03-19 15:21:18 -0700

    added template location

--- app/callToCScript.py
+++ app/callToCScript.py
@@ -7,11 +7,10 @@ import RunWordMacro
 OGMA_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 if OGMA_PATH not in sys.path:
     sys.path.append(OGMA_PATH)
-    
 
 
-def update_doc_properties(doc_path:str) -> None:
-    '''
+def update_doc_properties(doc_path: str) -> None:
+    """
     update_doc_properties runs the "UpdateDocumentProperties" macro in the word file at the given path
 
     Args:
@@ -19,18 +18,25 @@ def update_doc_properties(doc_path:str) -> None:
 
     Raises:
         cscriptError: _description_
-    '''
-    
+    """
+
     # set the macro
-    macro:str = r"ogmaMacro"
-    visible = False    
-    
-    RunWordMacro.run_word_macro(doc_path=doc_path, macro_name=macro, wordVisible=visible)
-        
+    macro: str = r"ogmaMacro"
+    template_path=r".\app\ogma.dotm"
+    visible = True
+
+    RunWordMacro.run_word_macro(
+        doc_path=doc_path,
+        macro_name=macro,
+        template_path=template_path,
+        wordVisible=visible,
+    )
+
     return
 
+
 if __name__ == "__main__":
-    from data.hidden.files import FILES # This can be removed
-    
-    # Example usage    
+    from data.hidden.files import FILES  # This can be removed
+
+    # Example usage
     update_doc_properties(doc_path=FILES[0])
