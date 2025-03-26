e11fd98cfa5b2db05ded7660d43aabd4ae73c00b
Aaron Shackelford <aaron.shackelford@wateraic.com>
2025-03-26 13:43:51 -0700

    Moving testing functions around

--- tests/ogmafunctest.py
+++ tests/ogmafunctest.py
@@ -6,8 +6,8 @@ OGMA_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "..",".."))
 if OGMA_PATH not in sys.path:
     sys.path.append(OGMA_PATH)
 
-from app.ogmaScripts.ogmaTestValues import modify_word_properties
-from app.ogmaScripts.documentPropertyUpdateTool import update_custom_document_properties
+from tests.ogmaTestValues import modify_word_properties
+from app.ogmaScripts.documentPropertyUpdateTool import document_properity_update_tool
 
 # MARK: Start Reading Here
 if __name__ == "__main__":    
@@ -16,4 +16,4 @@ if __name__ == "__main__":
     values: tuple[list[str], dict[str, str]] = modify_word_properties(file_paths=FILES)
     files: list[str] = values[0]
     props: dict[str, str] = values[1]
-    update_custom_document_properties(doc_paths=files, properties=props)
+    document_properity_update_tool(doc_paths=files, properties=props)
