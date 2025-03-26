5baab83bbe56e0b1a40946462eaafabd3fa01ffe
Aaron Shackelford <aaron.shackelford@wateraic.com>
2025-03-26 13:55:54 -0700

    adding static typing

--- app/ogmaGlobal.py
+++ app/ogmaGlobal.py
@@ -3,7 +3,9 @@
 
 import os
 
-APP_VERSION = "2.0.0"
+APP_VERSION:str = "2.0.0"
 
 # Locked access to word
 LOCK_FILE_PATH: str = os.path.join(os.path.abspath("."), os.path.join("tmp", "ogma_lock.lock"))
+
+VERBOSE_LEVEL:int = 0
