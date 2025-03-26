d5be42fbf68d04ca38ece5faa2bf34e329ff1e1c
Aaron Shackelford <aaron.shackelford@wateraic.com>
2025-03-26 16:33:29 -0700

    added tomarrow's todo to ogma

--- app/ogma.py
+++ app/ogma.py
@@ -10,6 +10,12 @@
  Ogma is a program that edits a word documents' propery values
 """
 
+# TODO
+# Package Ogma
+# Time Ogma
+#   1 file v 10 files
+# make script to increment the number of files it takes to run from 1 to len(FILES)
+# gather time inbetween and show AJ and BEN
 
 import argparse
 import json
